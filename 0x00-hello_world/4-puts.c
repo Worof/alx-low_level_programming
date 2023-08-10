97341598503d8cd90cda282afd93279ca7c78287
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-08-10 13:42:52 -0700

    C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
-int main(void) { puts("\"Programming is like building a multilingual puzzle"); return (0); }
 
+/**
+ * main - Prints a string using puts
+ *
+ * Return: Always returns 0, indicating success
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
