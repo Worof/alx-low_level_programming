5b0db270dee167e94319614692e3a369894cfdcc
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-08-11 16:51:05 -0700

    a program that prints the alphabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * main - A program to print the alphabet in lowercase followed by a new line
+ * main - A program to print the alphabet in lowercase
+ * followed by a new line.
  *
  * Return: Always 0 (Success)
 */
-int main(void) 
+int main(void)
 {
 char letter;
-/**
- * Loop to iterate through the alphabet and print each letter
-*/
+/* Loop to iterate through the alphabet and print each letter */
 for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
 putchar('\n');
