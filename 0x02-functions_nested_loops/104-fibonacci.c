13dc993ca2cb3e53defbe7eb84a9df755c131aa5
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-08-15 11:24:50 -0700

    a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ * Description: Calculates and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ * Numbers are separated by a comma and a space.
+ * Return: 0 if successful, non-zero otherwise
+ */
+int main(void)
+{
+	int i;
+	unsigned long int a = 1, b = 2, next, a1, a2, b1, b2;
+
+	printf("%lu, %lu", a, b); /* Print first two numbers */
+
+	for (i = 3; i <= 98; i++)
+	{
+		if (i <= 92) /* Within range of unsigned long int */
+		{
+			next = a + b;
+			a = b;
+			b = next;
+		}
+		else if (i == 93)
+		{
+			a1 = a / 10000000000; /* Splitting large numbers into parts */
+			a2 = a % 10000000000;
+			b1 = b / 10000000000;
+			b2 = b % 10000000000;
+
+			printf(", %lu%lu", b1, b2);
+		}
+		else /* Handling numbers larger than unsigned long int can hold */
+		{
+			a1 += b1;
+			a2 += b2;
+			if (a2 > 9999999999) /* Handling carry */
+			{
+				a2 -= 10000000000;
+				a1++;
+			}
+
+			printf(", %lu%lu", a1, a2);
+
+			b1 = a1;
+			b2 = a2;
+		}
+	}
+
+	printf("\n");
+	return (0);
+}
