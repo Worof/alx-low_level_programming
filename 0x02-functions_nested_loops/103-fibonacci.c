c83f53063a03f99cf0658fb12889b7932cc05b0e
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-08-15 11:19:15 -0700

    a program that finds and prints the sum of the even-valued terms, followed by a new line.

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
- * main - Calculate and print the sum of even Fibonacci numbers less than 4000000
- * 
- * The function initializes variables to represent the Fibonacci sequence and
- * iterates through the sequence, summing even values until the next term exceeds
- * 4000000. The sum of even terms is then printed to the console.
- *
+/**
+ * main - Main entry point for the program
+ * Description: Calculate and print the sum of even Fibonacci numbers
+ * less than 4000000. The function initializes variables to represent
+ * the Fibonacci sequence and iterates through the sequence, summing
+ * even values until the next term exceeds 4000000. The sum of even
+ * terms is then printed to the console.
  * Return: 0 if successful, non-zero otherwise
  */
 int main(void)
