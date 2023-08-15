736705b508ad3f815f2da1607507f491ed873de1
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-08-15 05:32:29 -0700

    Add print_alphabet_x10 function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,21 @@
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times, in lowercase.
+ *
+ * This function prints the alphabet, in lowercase letters, 10 times,
+ * each time followed by a new line. Utilizes the _putchar function.
+ * Complies with the Betty coding style.
+ */
 #include "main.h"
 
-/**
- * print_alphabet_x10 - prints the alphabet in lowercase 10 times, followed by newlines
-*/
 void print_alphabet_x10(void)
 {
-    for (int i = 0; i < 10; i++) {
-        for (char c = 'a'; c <= 'z'; c++) {
+    int i;
+    char c;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (c = 'a'; c <= 'z'; c++)
+        {
             _putchar(c);
         }
         _putchar('\n');
