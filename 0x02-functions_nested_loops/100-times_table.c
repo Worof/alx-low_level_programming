d9470ebc9043b0bf5b89c35b2e7284103afa3fbb
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-08-15 10:10:54 -0700

    a function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,29 +8,28 @@
  */
 void print_times_table(int n)
 {
-    int i, j, product;
+	int i, j, product;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            product = i * j;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			product = i * j;
+			if (j != 0)
+			{
+				if (product < 10)
+					printf("  ");  /* Three spaces for a single digit */
+				else if (product < 100)
+					printf(" ");   /* Two spaces for two digits */
+			}
+			printf("%d", product);
 
-            if (j != 0)
-            {
-                if (product < 10)
-                    printf("  ");  /* Three spaces for a single digit */
-                else if (product < 100)
-                    printf(" ");   /* Two spaces for two digits */
-            }
-            printf("%d", product);
-
-            if (j != n)
-                printf(", ");      /* Print comma and space */
-        }
-        printf("\n");
-    }
+			if (j != n)
+				printf(", ");      /* Print comma and space */
+		}
+		printf("\n");
+	}
 }
