c442d063b44baaa562e32cf6298e1dc226cb1548
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-08-15 04:36:36 -0700

    Added 0-putchar.c, a program to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _putchar - Writes a character to the standard output
+ * @c: The character to print
+ *
+ *  Return: On success 1.
+ *  On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c);
+/**
+ * main - Entry point for the program
+ *  Description: This function prints "_putchar" followed by a newline
+ *
+ *  Return: Returns 0 on success
+*/
+int main(void)
+{
+ _putchar('_');
+ _putchar('p');
+ _putchar('u');
+ _putchar('t');
+ _putchar('c');
+ _putchar('h');
+ _putchar('a');
+ _putchar('r');
+ _putchar('\n');
+ return (0);
+}
