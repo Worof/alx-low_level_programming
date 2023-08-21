873a7932b259b60e3e51033e6c8c45d13a9b431b
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-08-21 04:20:42 -0700

    Fixes formatting in 0-reset_to_98.c to pass Betty style check

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value it points to to 98
+ * @n: The pointer to an int we want to set to 98
+ *
+ * Return: No returning, just updating the value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
