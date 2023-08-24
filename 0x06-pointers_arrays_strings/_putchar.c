51b0ca8ff555a409cc3c6fe2809115496e776d4b
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-08-24 01:00:30 -0700

    a function that prints an integer

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
+ *
+ * Return: On success, 1 is returned. On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,4 +10,6 @@ char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
 char *rot13(char *str);
+void print_number(int n);
+int _putchar(char c);
 #endif /* MAIN_H */
