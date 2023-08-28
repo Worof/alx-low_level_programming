c538df7c71ed10d7af05d76e199eed385efbf00f
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-08-28 02:37:36 -0700

    a function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# 0x07. C - Pointers, arrays, and strings
+
+This project contains tasks related to pointers, arrays, and string manipulation in C programming.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+
+#endif /* MAIN_H */
