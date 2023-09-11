28ab2e3283039fd4c0b577a70a5b9e622b71c0b8
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-09-11 15:49:58 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Describes a dog's basic information
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: Longer description (if necessary)
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
