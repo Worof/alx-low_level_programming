09bb4745eafac9e39fb726b00e6caeef73be981e
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-12-21 14:42:46 -0800

    a function that adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,50 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table you want to add or update the key/value to.
+ * @key: The key. Cannot be an empty string.
+ * @value: The value associated with the key. Must be duplicated.
+ *         Can be an empty string.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    unsigned long int index;
+    hash_node_t *new_node, *temp_node;
+
+    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+        return (0);
+
+    index = key_index((const unsigned char *)key, ht->size);
+    temp_node = ht->array[index];
+    while (temp_node != NULL)
+    {
+        if (strcmp(temp_node->key, key) == 0)
+        {
+            free(temp_node->value);
+            temp_node->value = strdup(value);
+            return (1);
+        }
+        temp_node = temp_node->next;
+    }
+
+    new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+        return (0);
+
+    new_node->key = strdup(key);
+    new_node->value = strdup(value);
+    if (new_node->key == NULL || new_node->value == NULL)
+    {
+        free(new_node->key);
+        free(new_node->value);
+        free(new_node);
+        return (0);
+    }
+
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
+    return (1);
+}
